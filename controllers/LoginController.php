b2a4341fccb23da9f19a1fdfcfff3ebb4945a5a6
Revsky <angel1vengador100@gmail.com>
2022-09-28 11:31:19 -0500

    Creando rutas del index para crear usuarios, iniciar sesión, recuperar contraselas

---
+++ controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace Controllers;
+
+class LoginController{
+
+    public static function login( ) 
+    {
+        echo "dede login";
+    }
+
+    public static function logout( ) 
+    {
+        echo "dede logout";
+    }
+
+    public static function olvide( ) 
+    {
+        echo "dede olvide";
+    }
+
+    public static function recuperar( ) 
+    {
+        echo "dede recuperar";
+    }
+
+    public static function crear( ) 
+    {
+        echo "dede crear";
+    }
+}
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'angel/app-salon_mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '60343120b910459db3fc0af2a4c1e44a7dd2564a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'angel/app-salon_mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '60343120b910459db3fc0af2a4c1e44a7dd2564a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
