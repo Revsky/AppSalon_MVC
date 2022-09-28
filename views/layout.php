39b7a94e9aa0874d0ef3c0122224e5ff51bdf9e0
Revsky <angel1vengador100@gmail.com>
2022-09-28 11:53:18 -0500

    Dando estilos al titulo de login

--- views/layout.php
+++ views/layout.php
@@ -9,7 +9,12 @@
 </head>
 <body>
 
-    <?php echo $contenido; ?>
+    <div class="contenedor-app">
+        <div class="imagen"></div>
+        <div class="app">
+            <?php echo $contenido; ?>
+        </div>
+    </div>
             
 </body>
 </html>
