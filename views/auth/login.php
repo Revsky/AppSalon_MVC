739099364db500b86a5616d14f432ce20a70496e
Revsky <angel1vengador100@gmail.com>
2022-09-28 11:58:38 -0500

    Creando formulario de login y opciónes de creación y recuperación con HTML

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,31 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia Sesión</p>
+
+<form class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+            type="email"
+            id="email"
+            placeholder="correo@correo.com"
+            name="email"
+        />
+    </div>
+
+    <div class="campo">
+        <label for="password">Password</label>
+        <input 
+            type="password"
+            id="password"
+            placeholder="Token"
+            name="password"
+        />
+    </div>
+
+    <input type="submit" class="boton" value="Entrar">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes una cuenta? Crea una</a>
+    <a href="/olvide">¿Olvidaste tu Constraseña?</a>
+</div>
