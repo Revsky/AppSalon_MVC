04661765c217ab1a1c4e8f8687af9158ab6d6b67
Revsky <angel1vengador100@gmail.com>
2022-10-03 23:03:47 -0500

    Creando los elementos de active record del modelo usuarios en el archivo Usuario.php

---
+++ models/Usuario.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace Model;
+
+class Usuario extends ActiveRecord{
+
+    // Base de datos
+    protected static $tabla = 'usuarios';
+    protected static $columnasDb = ['id','nombre','apellido','email','password','telefono','admin','confirmado','token'];
+
+    public $id;
+    public $nombre;
+    public $apellido;
+    public $email;
+    public $password;
+    public $telefono;
+    public $admin;
+    public $confirmado;
+    
+    public function __construct( $args = [] )
+    {       
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? null;
+        $this->apellido = $args['apellido'] ?? null;
+        $this->email = $args['email'] ?? null;
+        $this->password = $args['password'] ?? null;
+        $this->telefono = $args['telefono'] ?? null;
+        $this->admin = $args['admin'] ?? null;
+        $this->confirmado = $args['confirmado'] ?? null;
+        
+    }
+
+}
+
+?>
