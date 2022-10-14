a27314eb8eb6e0f8095ac4e3bae52a237e6ba717
Revsky <angel1vengador100@gmail.com>
2022-10-14 13:00:46 -0500

    Validando el login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,6 +1,10 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia Sesión</p>
 
+<?php 
+    include_once __DIR__ . "/../template/alertas.php";
+?>
+
 <form class="formulario" method="POST" action="/">
     <div class="campo">
         <label for="email">Email</label>
