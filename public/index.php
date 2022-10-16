56f09ee2080f8a30799b2144dbd42f30fd2df820
Revsky <angel1vengador100@gmail.com>
2022-10-16 12:04:50 -0500

    Creand seccion para crear una nuea cita

--- public/index.php
+++ public/index.php
@@ -2,8 +2,10 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\CitaController;
 use MVC\Router;
 use Controllers\LoginController;
+
 
 $router = new Router();
 
@@ -27,6 +29,9 @@ $router->post('/crear-cuenta',[LoginController::class,'crear']);
 $router->get('/confirmar-cuenta',[LoginController::class,'confirmar']);   
 // Condirma cuenta
 $router->get('/mensaje',[LoginController::class,'mensaje']);   
+
+// Area privada
+$router->get('/cita',[CitaController::class,'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
---
+++ views/cita/index.php
@@ -0,0 +1,2 @@
+<h1 class="nombre-pagina">Nueva Cita</h1>
+<p class="descripcion-pagina">Elige tu servicios y coloca tus datos</p>
