c74ca9537e561270a4ec476740fdb747a9dc3075
Revsky <angel1vengador100@gmail.com>
2022-10-18 11:00:54 -0500

    Creando logica para hacer mas intuitiva la interfaz del selector

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded',function(){
 })
 
 function iniciarApp(){
+    mostrarSeccion(); // <- Muestra y oculta las secciones
     tabs(); // <- Cambia la sección cuando se presionen los tabs
 }
 function mostrarSeccion(){
@@ -21,6 +22,17 @@ function mostrarSeccion(){
     const seccion = document.querySelector(pasoSelector);
 
     seccion.classList.add('mostrar')
+
+    // Quita la clase actual al tab anterior
+    const tabAnterior = document.querySelector('.actual')
+    if(tabAnterior){
+        tabAnterior.classList.remove('actual')
+    }
+
+    // Resalta el tab actual
+    /// El selector usa [] dado que la propiedad del tag HTML es una propiead personal, es decir, no existe en HTML tal cual si no que nosotros la hemos integrado
+    const tab = document.querySelector(`[data-paso="${paso}"]`) // data-paso = 1 or 2 or 3
+    tab.classList.add('actual')
 
 }
 
