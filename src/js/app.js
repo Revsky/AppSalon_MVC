8646e221fb5e8774fda6f5e62b185b37aba68c84
Revsky <angel1vengador100@gmail.com>
2022-10-18 10:54:17 -0500

    Agregar código para mostrar y ocultar secciones

--- src/js/app.js
+++ src/js/app.js
@@ -7,10 +7,23 @@ document.addEventListener('DOMContentLoaded',function(){
 function iniciarApp(){
     tabs(); // <- Cambia la sección cuando se presionen los tabs
 }
-
 function mostrarSeccion(){
-    console.log("Mostrando seccion")
+
+    // Ocultar la seccion que tenga la clase mostrar
+    const seccionAnterior = document.querySelector('.mostrar') // <- el . para seleccionar clases es necesario solo para ql querySelector
+    if(seccionAnterior){
+        seccionAnterior.classList.remove('mostrar')
+
+    }
+    
+    // Seleccionar la seccion con el paso
+    const pasoSelector = `#paso-${paso}` //<- Podemos usar template string para fucionar HTML + PHP
+    const seccion = document.querySelector(pasoSelector);
+
+    seccion.classList.add('mostrar')
+
 }
+
 
 function tabs(){
     const botones = document.querySelectorAll('.tabs button');
