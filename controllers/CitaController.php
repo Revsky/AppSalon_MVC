69730466811173bca44d21f463fa022518400fb7
Revsky <angel1vengador100@gmail.com>
2022-11-22 12:45:28 -0600

    Validando que el usuario este autenticado para ver esta vista

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -8,9 +8,13 @@ class CitaController{
 
     public static function index( Router $router )
     {
-        if(!isset($_SESSION['nombre'])){
-            header('Location: /');
-        }
+        //if(!isset($_SESSION['nombre'])){
+        //    header('Location: /');
+        //}
+
+        session_start();
+        // Si no esta autenticado, lo redireccionamos a /
+        isAuth();
 
         $router->render('cita/index',[
             'nombre' => $_SESSION['nombre'],
--- includes/funciones.php
+++ includes/funciones.php
@@ -12,3 +12,10 @@ function sanitizar($html) : string {
     $s = htmlspecialchars($html);
     return $s;
 }
+
+// Funcion que revisa que el usuario esta autenticado
+function isAuth():void{
+    if(!isset($_SESSION['login'])){
+        header('Location:/');
+    }
+}
